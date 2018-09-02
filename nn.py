c3c085fa81b6d50412fd6d68bd7ce175961f5cce
chandru <chandru@chandra.cm.cluster>
2018-09-03 01:06:15 +0530

    minor_edits

--- nn.py
+++ nn.py
@@ -22,8 +22,8 @@ class nn:
 	def initialize(self):
 		self.weight=np.random.normal(0,1/np.sqrt(self.width),np.shape(self.weight))
 		self.bias=bias_const*np.ones(np.shape(self.bias),dtype=float)
-		self.weight[self.num_class:self.width,:,self.depth-1]=np.zeros((self.width-self.num_class,self.width,1),dtype=float)
-		self.bias[self.num_class:self.width,self.depth-1]=np.inf((self.width-self.num_class,1),dtype=float)
+		self.weight[self.num_class:self.width-1,:,self.depth-1]=np.zeros((self.width-self.num_class,self.width,1),dtype=float)# the paths after num_classes are killed by making weights zero and biases -infinity
+		self.bias[self.num_class:self.width-1,self.depth-1]=-np.inf((self.width-self.num_class,1),dtype=float) 
 		
 	def fp(self,xin):#Forward Propagation
 		o=xin
@@ -35,23 +35,20 @@ class nn:
 				a=np.ones((self.width,1),dtype=float)
 			self.output[:,i]=o
 			self.active[:,i]=a
-		self.logit=self.output[:,self]
+		self.logit=self.output[:,self.depth-1]
 		return self.logit
-	def bp(self,xin,yin):#Backward Propagation: yin is in one-hot format
+	def bp(self,xin,yin):#Backward Propagation: yin is not in one-hot format; yin is the label of the class from o to num_class-1
 		o=self.fp(self,xin)
 		p=np.exp(o)
 		p=p/np.sum(p)
 		delta=-p
 		delta[yin]=delta[yin]+1.0
-		delta_weight=np.zeros(np.shape(self.weight),dtype=float)
-                delta_bias=np.zeros(np.shape(self.bias),dtype=float)
-		delta_bias_last=delta
-		delta_weight_last=np.tile(delta,self.width)*np.diag(self.output[:,self.depth-1])
-		weight_prev=self.weight_last	
+		weight_prev=np.identity(self.width,dtype=float)	
 		for i in xrange (self.depth-1,-1,0):
-			delta_bias[:,i]=np.transpose(weight_prev)*delta*self_active[:,i]
-			delta_weight[:,i]=np.tile(delta_bias,self.width)*np.diag(self.output[:,i-1])
-			weight_prev=self.weight[:,i]	
+			self.delta_bias[:,i]=np.transpose(weight_prev)*delta*self_active[:,i]
+			self.delta_weight[:,i]=np.tile(delta_bias,self.width)*np.diag(self.output[:,i-1])
+			weight_prev=self.weight[:,i]
+			delta=self.delta_bias[:,i]	
 		i=0
 		delta_bias[:,i]=np.transpose(weight_prev)*delta*self_active[:,i]
                 delta_weight[:,i]=np.tile(delta_bias,self.width)*np.diag(xin)
